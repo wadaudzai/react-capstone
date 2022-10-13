527bf14872e8db510ae90182b7c32faae587b7c1
wadaudzai <wadaudzai@gmail.com>
2022-10-14 02:17:07 +0500

    Fix linter warning since it was considered an error by netlify.

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,4 +1,5 @@
 /* eslint-disable max-len */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { React, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import OneCoin from './OneCoin';
